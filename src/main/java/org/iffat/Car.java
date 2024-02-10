ce9e07ce6b78ab8f515bae04856caacde98f3a61
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-10 07:39:08 +0700

    01. Introduction Class and Objects

---
+++ src/main/java/org/iffat/Car.java
@@ -0,0 +1,17 @@
+package org.iffat;
+
+public class Car {
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+    public void describeCar() {
+        System.out.println(doors + " -Doors " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "convertible" : ""));
+    }
+}
