6e57da101e9f52d565c63b2c99a6aa21f11d4547
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-10 08:49:49 +0700

    04. Classes challenge exercise

---
+++ src/main/java/org/iffat/classes_challange/Main.java
@@ -0,0 +1,25 @@
+package org.iffat.classes_challange;
+
+public class Main {
+    public static void main(String[] args) {
+        Account bobsAccount = new Account();
+
+        bobsAccount.setNumber("12345");
+        bobsAccount.setBalance(1000);
+        bobsAccount.setCustomerName("Bob Brown");
+        bobsAccount.setCustomerEmail("myemail@gmail.com");
+        bobsAccount.setCustomerPhone("(087) 123-4567");
+
+        bobsAccount.withdrawFunds(100);
+        bobsAccount.depositFunds(250);
+        bobsAccount.withdrawFunds(50);
+
+        bobsAccount.withdrawFunds(200);
+
+        bobsAccount.depositFunds(100);
+        bobsAccount.withdrawFunds(45.55);
+        bobsAccount.withdrawFunds(54.46);
+
+        bobsAccount.withdrawFunds(54.45);
+    }
+}
