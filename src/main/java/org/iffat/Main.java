8e7dfbf1a9aacb62ac29428ea50b42e0de50f47d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-10 07:50:17 +0700

    02. Introduction Class Using Getter Methods

--- src/main/java/org/iffat/Main.java
+++ src/main/java/org/iffat/Main.java
@@ -2,6 +2,9 @@ package org.iffat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car();
+        System.out.println("Make = " + car.getMake());
+        System.out.println("Model = " + car.getModel());
+        car.describeCar();
     }
 }
