dbcfe207ed789e869f69455a785e150b5f45ae2d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-02-12 11:45:18 +0700

    27. OOP Master Challenge

---
+++ src/main/java/org/iffat/bills_burger_challenge/Main.java
@@ -0,0 +1,32 @@
+package org.iffat.bills_burger_challenge;
+
+public class Main {
+    public static void main(String[] args) {
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
+
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder mealOrder = new MealOrder();
+//        mealOrder.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        mealOrder.setDrinkSize("LARGE");
+//        mealOrder.printItemizedList();
+
+//        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+//        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+    }
+}
